644b2370c313a847d9d197f6d4e2672ac73bd605
Connor Braa <cbraa@pivotal.io>
2018-10-23 07:38:23 -0700

    improve graphing a bit, start checking in data

--- graph.py
+++ graph.py
@@ -3,15 +3,17 @@ import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('~/Documents/postgres-unindexed.csv', header=0, encoding='utf-8')
+filename = 'data/postgres-linearx0.csv'
+data = pd.read_csv(filename, header=0, encoding='utf-8')
 
 sns.set()
 sns.set_style("darkgrid")
 
-ax = sns.lineplot(x="labels", y="duration", hue="hue", data=data)
+ax = sns.lineplot(x="label_count", y="duration", hue="hue", data=data)
 # ax = sns.lmplot(x='app_count',y='time',data=data, hue="label", fit_reg=True)
 ax.set(xlabel="label_count", ylabel="Time (seconds)")
-ax.set_title('labelin')
+ax.set_title(filename)
 #ax.set_xscale('log')
 
 plt.show()
+plt.savefig(filename+'.svg', type='svg')
